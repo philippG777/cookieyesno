e5861fb1524db17c36721b745b2d5ce81d9a6c0c
philippG777 <philippG777@protonmail.com>
2020-05-30 16:51:34 +0200

    Create custom rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,68 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import buble from '@rollup/plugin-buble';
+import { uglify } from 'rollup-plugin-uglify';
 
-// `npm run build` -> `production` is true
-// `npm run dev` -> `production` is false
 const production = !process.env.ROLLUP_WATCH;
 
-// const srcFile = (production)? 'src/cookieyesno.js' : 'src/main.js';
-// const resultFile = (production)? 'dist/cookieyesno.min.js' : 'public/bundle.js';
 
 const devBuildConfig = {
-	input: 'src/main.js',
+	input: 'src/index.js',
 	output: {
-		file: 'public/bundle.js',
-		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+		file: 'dev/bundle.js',
+		format: 'iife',
 		sourcemap: true
 	},
 	plugins: [
-		resolve(), // tells Rollup how to find date-fns in node_modules
-		commonjs(), // converts date-fns to ES modules
-		production && terser() // minify, but only in production
+        resolve(),
+        commonjs(),
+        buble()
 	]
 };
 
-const productionBuildConfig = // browser-friendly UMD build
-{
-    input: 'src/cookieyesno.js',
-    output: {
-        name: 'cookieyesno',
-        file: 'dist/cookieyesno.js',
-        format: 'iife',
-        sourcemap: false
+const productionBuildConfig = [
+    {
+        input: 'src/cookieyesno.js',
+        output: {
+            file: 'dist/cookieyesno.js',
+            name: 'CookieYesNo',
+            sourcemap: false,
+            format: 'iife'
+        },
+        plugins: [
+            resolve(), // so Rollup can find `ms`
+            commonjs(), // so Rollup can convert `ms` to an ES module
+            buble()
+        ]
     },
-    plugins: [
-        resolve(), // so Rollup can find `ms`
-        commonjs(), // so Rollup can convert `ms` to an ES module
-        terser()
-    ]
-};
+    {
+        input: 'src/cookieyesno.js',
+        output: {
+            file: 'dist/cookieyesno.min.js',
+            name: 'CookieYesNo',
+            sourcemap: false,
+            format: 'iife'
+        },
+        plugins: [
+            resolve(), // so Rollup can find `ms`
+            commonjs(), // so Rollup can convert `ms` to an ES module
+            buble(),
+            uglify()
+        ]
+    },
+    {
+        input: 'src/cookieyesno.js',
+        output: {
+            file: 'dist/cookieyesno.cjs.js',
+            name: 'CookieYesNo',
+            sourcemap: false,
+            format: 'cjs'
+        },
+        plugins: [
+            resolve(),
+            commonjs()
+        ]
+    }
+];
 
 export default (production)? productionBuildConfig : devBuildConfig;
