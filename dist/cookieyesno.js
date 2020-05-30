063bee452f63204f11ae2ee6b910ef69ea4665f6
philippG777 <philippG777@protonmail.com>
2020-05-30 16:52:33 +0200

    Add dist files

--- dist/cookieyesno.js
+++ dist/cookieyesno.js
@@ -1,4 +1,233 @@
-var cookieyesno=function(){"use strict";
-/*!
-    CookieYesNo | philippG777 | https://github.com/philippG777/cookieyesno | BSD 3-Clause License
-    */return class{constructor(e){this.cookie={set:function(e,t){let n=new Date;n.setTime(n.getTime()+7776e6),document.cookie=e+"="+t+";expires="+n.toUTCString()+";path=/"},get:function(e){const t=document.cookie.split(";");for(let n=0;n<t.length;n++){const s=t[n].trim().split("=");if(s[0]==e)return s[1]}return null}},this._config=e,this._changeListeners=[],this._acceptListeners=[],this._rejectListeners=[],this.banner=this._createBanner(),this._applyStyle(),this._addListeners(),null==this._load()?this.show():this._runAcceptRejectListeners()}_load(){const e=this.cookie.get("_cyn");return null==e?null:JSON.parse(e)}_save(e){this.cookie.set("_cyn",JSON.stringify(e))}onChange(e){this._changeListeners.push(e)}onAccept(e,t){!0===this.getSettings()[e]?t():this._acceptListeners.push({name:e,cb:t})}onReject(e,t){!1===this.getSettings()[e]?t():this._rejectListeners.push({name:e,cb:t})}getSettings(){const e=this._load();return null==e?{}:e}reviewSettings(){this._bannerApplySettings(this.getSettings()),this.show()}_createBanner(){const e=document.createElement("div");e.className="cyn-banner";let t='<h3 style="font-size: 28px; font-weight: bold;">This site uses cookies</h3>';t+="<p>"+this._config.text+"</p>",t+='<table class="cyn-categories"><tbody>';for(const e in this._config.categories){const n=this._config.categories[e];t+='<tr><td style="font-weight:bold;">'+e+"</td><td>"+n.description+"</td><td>",t+='<input type="checkbox" value="'+e+'"'+(n.allowed?" checked":"")+(!0===n.changeable||void 0===n.changeable?"":" disabled")+"/>",t+="</td></tr>"}return t+="</tbody></table>",t+='<p>For detailed information take a look at the <a href="'+this._config.cookiePolicy+'">Cookie Policy</a>.</p>',t+='<button class="cyn-btn-save">Save Settings</button>',t+='<button class="cyn-btn-accept-all">Accept all</button>',e.innerHTML=t,e.style.display="none",document.body.appendChild(e),e}_bannerApplySettings(e){let t=this.banner.getElementsByTagName("input");for(let n=0;n<t.length;n++)"checkbox"==t[n].type&&(t[n].checked=null!=e[t[n].value.toLowerCase()]&&1==e[t[n].value.toLowerCase()])}_applyStyle(){let e=this.banner.style;e.zIndex=5e3,e.backgroundColor="#fff",e.position="fixed",e.padding="16px",e.boxShadow="0 0 24px #aaa",e.borderRadius="8px",window.innerWidth<=768?e.right=e.left=e.bottom="16px":e.right=e.bottom="32px",e=this.banner.getElementsByTagName("table")[0].style,e.margin="16px";const t=this.banner.getElementsByTagName("td");for(let e=0;e<t.length;e++)t[e].style.padding="8px";const n=this.banner.getElementsByTagName("button");for(let t=0;t<n.length;t++)e=n[t].style,e.boxSizing="border-box",e.cursor="pointer",e.margin="8px",e.marginTop="24px",e.borderRadius="4px","cyn-btn-accept-all"==n[t].className?(e.border="0px",e.backgroundColor="#48c774",e.color="white",e.fontWeight="bold",e.fontSize="24px",e.padding="12px",e.paddingLeft=e.paddingRight="16px"):(e.backgroundColor="white",e.color="#999",e.border="2px solid #bbb",e.fontSize="20px",e.padding="8px")}_addListeners(){const e=this.banner.getElementsByTagName("button");for(let t=0;t<e.length;t++)e[t].addEventListener("click",this._onButtonClick.bind(this))}_onButtonClick(e){let t={};const n=e.target.className;if(-1!=n.indexOf("cyn-btn-accept-all"))for(const e in this._config.categories)t[e.toLowerCase()]=!0;else{if(-1==n.indexOf("cyn-btn-save"))return;{const e=this.banner.getElementsByTagName("input");for(let n=0;n<e.length;n++)"checkbox"==e[n].type&&(t[e[n].value.toLowerCase()]=e[n].checked)}}this.hide();for(let e=0;e<this._changeListeners.length;e++)this._changeListeners[e](t);this._runAcceptRejectListeners(t),this._save(t)}_runAcceptRejectListeners(e){void 0===e&&(e=this.getSettings());for(let t=0;t<this._acceptListeners.length;t++)!0===e[this._acceptListeners[t].name]&&this._acceptListeners[t].cb();for(let t=0;t<this._rejectListeners.length;t++)!1===e[this._rejectListeners[t].name]&&this._rejectListeners[t].cb()}show(){this.banner.style.display="block"}hide(){this.banner.style.display="none"}}}();
+var CookieYesNo = (function () {
+    'use strict';
+
+    /*!
+    CookieYesNo | philippG777 | https://github.com/philippG777/cookieyesno | MIT License
+    */
+
+    var CookieYesNo = function CookieYesNo(config) {
+        this.cookie = { //  cookie handler
+            set: function(cname, data) {
+                var d = new Date();
+                d.setTime(d.getTime() + 90 * 86400000);  // 90 days
+                document.cookie = cname + '=' + data + ';expires=' + d.toUTCString() + ';path=/';
+            },
+            get: function(cname) {
+                var parts = document.cookie.split(';');
+
+                for(var i = 0; i < parts.length; i++) {
+                    var pair = parts[i].trim().split('=');
+                    if(pair[0] == cname) { return pair[1]; }
+                }
+                return null;
+            }
+        };
+
+        this._config = config;
+
+        // listeners
+        this._changeListeners = [];
+        this._acceptListeners = [];  
+        this._rejectListeners = [];      
+
+        this.banner = this._createBanner();
+        this._applyStyle();
+        this._addListeners();
+
+        if(this._load() == null)
+            { this.show(); }
+        else
+            { this._runAcceptRejectListeners(); }
+    };
+
+    CookieYesNo.prototype._load = function _load () {
+        var data = this.cookie.get('_cyn');
+        return (data == null)? null : JSON.parse(data);
+    };
+
+    CookieYesNo.prototype._save = function _save (settings) {
+        this.cookie.set('_cyn', JSON.stringify(settings));
+    };
+
+    CookieYesNo.prototype.onChange = function onChange (cb) {
+        this._changeListeners.push(cb);
+    };
+
+    CookieYesNo.prototype.onAccept = function onAccept (name, cb) {
+        if((this.getSettings())[name] === true)
+            { cb(); }
+        else
+            { this._acceptListeners.push({
+                name: name,
+                cb: cb
+            }); }
+    };
+
+    CookieYesNo.prototype.onReject = function onReject (name, cb) {
+        if((this.getSettings())[name] === false)
+            { cb(); }
+        else
+            { this._rejectListeners.push({
+                name: name,
+                cb: cb
+            }); }
+    };
+
+    CookieYesNo.prototype.getSettings = function getSettings () {
+        var settings = this._load();
+        if(settings == null) { return {}; }
+        else { return settings; }
+    };
+
+    CookieYesNo.prototype.reviewSettings = function reviewSettings () {
+        this._bannerApplySettings(this.getSettings());
+        this.show();
+    };
+
+    CookieYesNo.prototype._createBanner = function _createBanner () {
+        var el = document.createElement('div');
+        el.className = 'cyn-banner';
+        var text = '<h3 style="font-size: 28px; font-weight: bold;">This site uses cookies</h3>';
+        text += '<p>' + this._config.text + '</p>';
+        text += '<table class="cyn-categories"><tbody>';
+
+        for(var key in this._config.categories) {
+            var cat = this._config.categories[key];
+
+            text += '<tr><td style="font-weight:bold;">' + key + '</td><td>' + cat.description + '</td><td>';
+            text += '<input type="checkbox" value="' + key + '"'
+                 + ((cat.allowed)? ' checked' : '')
+                 + ((cat.changeable === true || cat.changeable === undefined)? '' : ' disabled') + '/>';
+            text += '</td></tr>';
+        }
+
+        text += '</tbody></table>';
+
+        // Cookie Policy link
+        text += '<p>For detailed information take a look at the <a href="' + this._config.cookiePolicy +
+            '">Cookie Policy</a>.</p>';
+
+        text += '<button class="cyn-btn-save">Save Settings</button>';
+        text += '<button class="cyn-btn-accept-all">Accept all</button>';
+
+        el.innerHTML = text;
+        el.style.display = 'none';
+
+        document.body.appendChild(el);
+        return el;
+    };
+
+    CookieYesNo.prototype._bannerApplySettings = function _bannerApplySettings (settings) {
+        var inputs = this.banner.getElementsByTagName('input');
+        for(var i = 0; i < inputs.length; i++)
+            { if(inputs[i].type == 'checkbox')
+            {
+                inputs[i].checked = (settings[inputs[i].value.toLowerCase()] != undefined &&
+                    settings[inputs[i].value.toLowerCase()] == true)? true : false;
+            } }
+    };
+
+    CookieYesNo.prototype._applyStyle = function _applyStyle () {
+        // banner
+        var style = this.banner.style;
+        style.zIndex = 5000;            // move to front
+        style.backgroundColor = '#fff'; // white background
+        style.position = 'fixed';
+            
+        style.padding = '16px';
+        style.boxShadow = '0 0 24px #aaa';
+        style.borderRadius = '8px';
+            
+        if(window.innerWidth <= 768)// mobile
+            { style.right = style.left = style.bottom = '16px'; }
+        else    // desktop (large screen)
+            { style.right = style.bottom = '32px'; }
+
+        // table
+        style = this.banner.getElementsByTagName('table')[0].style;
+        style.margin = '16px';
+
+        var td = this.banner.getElementsByTagName('td');
+
+        for(var i = 0; i < td.length; i++)
+            { td[i].style.padding = '8px'; }
+
+        // buttons
+        var buttons = this.banner.getElementsByTagName('button');
+        for (var i$1 = 0; i$1 < buttons.length; i$1++) {
+            style = buttons[i$1].style;
+                
+            style.boxSizing = 'border-box';
+            style.cursor = 'pointer';
+            style.margin = '8px';
+            style.marginTop = '24px';
+            style.borderRadius = '4px';
+
+            if(buttons[i$1].className == 'cyn-btn-accept-all') {
+                style.border = '0px';
+                style.backgroundColor = '#48c774';
+                style.color = 'white';
+                style.fontWeight = 'bold';
+                style.fontSize = '24px';
+                style.padding = '12px';
+                style.paddingLeft = style.paddingRight = '16px';
+            } else {
+                style.backgroundColor = 'white';
+                style.color = '#999';
+                style.border = '2px solid #bbb';
+                style.fontSize = '20px';
+                style.padding = '8px';
+            }
+        }
+    };
+
+    CookieYesNo.prototype._addListeners = function _addListeners () {
+        var buttons = this.banner.getElementsByTagName('button');
+        for(var i = 0; i < buttons.length; i++)
+            { buttons[i].addEventListener('click', this._onButtonClick.bind(this)); }
+    };
+        
+    CookieYesNo.prototype._onButtonClick = function _onButtonClick (e) {
+        var newSettings = {};
+
+        var className = e.target.className;
+        if(className.indexOf('cyn-btn-accept-all') != -1) {
+            for(var category in this._config.categories)
+                { newSettings[category.toLowerCase()] = true; } // turn Analytics to analytics
+        } else if(className.indexOf('cyn-btn-save') != -1) {
+            var inputs = this.banner.getElementsByTagName('input');
+            for (var i  = 0; i < inputs.length; i++)
+                { if(inputs[i].type == 'checkbox')
+                    { newSettings[inputs[i].value.toLowerCase()] = inputs[i].checked; } }
+        } else { return; }
+
+        this.hide();
+
+        // run CBs
+        for(var i$1 = 0; i$1 < this._changeListeners.length; i$1++)
+            { this._changeListeners[i$1](newSettings); }
+            
+        this._runAcceptRejectListeners(newSettings);
+
+        this._save(newSettings);
+    };
+
+    CookieYesNo.prototype._runAcceptRejectListeners = function _runAcceptRejectListeners (settings) {
+        if(settings === undefined)  // default parameter
+            { settings = this.getSettings(); }
+
+        for(var i = 0; i < this._acceptListeners.length; i++)
+            { if(settings[this._acceptListeners[i].name] === true)
+                { this._acceptListeners[i].cb(); } }
+
+        for(var i$1 = 0; i$1 < this._rejectListeners.length; i$1++)
+            { if(settings[this._rejectListeners[i$1].name] === false)
+                { this._rejectListeners[i$1].cb(); } }
+    };
+
+    CookieYesNo.prototype.show = function show () { this.banner.style.display = 'block'; };
+    CookieYesNo.prototype.hide = function hide () { this.banner.style.display = 'none'; };
+
+    return CookieYesNo;
+
+}());
